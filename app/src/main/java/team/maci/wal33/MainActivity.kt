2627c88da8ae5ee84af4c8a290797b5054b0f960
David Foldesi <david.foldesi@doclerholding.com>
2021-08-13 12:01:16 +0200

    Init commit

---
+++ app/src/main/java/team/maci/wal33/MainActivity.kt
@@ -0,0 +1,12 @@
+package team.maci.wal33
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
